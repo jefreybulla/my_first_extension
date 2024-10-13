dc1fc9db7787792d5141af59184f13713bd1823e
Jefrey Bulla <jefrey.bulla@gmail.com>
2024-10-13 09:30:34 -0400

    made first edit to extension

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,22 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette } from '@jupyterlab/apputils'
+
 /**
  * Initialization data for the jupyterlab_apod extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab_apod:plugin',
+  id: 'jupyterlab-apod',
   description: 'Show a random NASA Astronomy Picture of the Day in a JupyterLab panel',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab_apod is activated!');
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    console.log('Hello world: JupyterLab extension jupyterlab_apod is activated!')
+    console.log('ICommandPalette:', palette)
+    console.log('Hi Jef')
+    console.log('New console out')
   }
-};
+}
 
 export default plugin;
